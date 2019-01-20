9efbce867dd713e7a03cf18def4f988250e87a0f
firehare <ubuntu.firehare@gmail.com>
2019-01-20 14:22:09 +0800

    添加演示数据

---
+++ database/seeds/UserSeeder.js
@@ -0,0 +1,36 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| UserSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+const User = use('App/Models/User')
+
+class UserSeeder {
+  async run() {
+    const users = [{
+      username: '张三',
+      email: 'z3@ninghao.net',
+      password: '111111'
+    }, {
+      username: '李四',
+      email: 'l4@ninghao.net',
+      password: '111111'
+    }, {
+      username: '王二',
+      email: 'w2@ninghao.net',
+      password: '111111'
+    }]
+    User.createMany(users)
+  }
+}
+
+module.exports = UserSeeder
